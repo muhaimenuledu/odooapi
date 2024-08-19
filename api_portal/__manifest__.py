33e5da62bd868e4c8c11f39b3def9e50625c91eb
Muhaimenul Islam <90051115+muhaimenuledu@users.noreply.github.com>
2024-08-19 19:05:26 +0600

    Update __manifest__.py

--- api_portal/__manifest__.py
+++ api_portal/__manifest__.py
@@ -1,1 +1,18 @@
+{
+    'name': 'API PORTAL',
+    'version': '1.0',
+    'author': 'MUHAIMENUL',
+    'category': 'Technical',
+    'depends': ['base', 'mail', 'uom', "sale", "hr"],
 
+    'description': """
+    """,
+    'data': [
+                'security/ir.model.access.csv',
+                'views/portal_data_views.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+    'license': 'LGPL-3',
+}
